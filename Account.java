64c4b43e02a90a97c9d84fa130485d4057bf8932
Mohini <161958801+mohini613@users.noreply.github.com>
2024-09-02 13:27:38 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,17 +1,17 @@
 package com.CustomerAcc;
 
-import java.util.List;
-import java.util.Random;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "account_details")
 public class Account {
- @Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private int AccountNumber;
 
     private String IFSC_Code;
@@ -21,16 +21,7 @@ public class Account {
     private Customer customer;
     
     public Account() {
-        this.AccountNumber = generateAccountNumber();
     }
-
-    // Method to generate a random account number
-    private int generateAccountNumber() {
-        Random random = new Random();
-        return 10000000 + random.nextInt(90000000); // Generates an 8-digit random number
-    }
-
-    
 
     public int getAccountNumber() {
         return AccountNumber;
@@ -56,73 +47,11 @@ public class Account {
         BankName = bankName;
     }
 
-public Customer getCustomer() {
-    return customer;
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
-
-public void setCustomer(Customer customer) {
-    this.customer = customer;
-}
-}
-
-/*package com.CustomerAcc;
-
-import java.util.List;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-
-@Entity
-public class Account {
-	
-	@OneToMany(mappedBy = "Account")
-	private List<Customer> Customer;
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private int AccountNumber;
-
-private String IFSC_Code;
-
-private String BankName;
-
-public List<Customer> getCustomer() {
-	return Customer;
-}
-
-public void setCustomer(List<Customer> customer) {
-	Customer = customer;
-}
-
-public int getAccountNumber() {
-	return AccountNumber;
-}
-
-public void setAccountNumber(int accountNumber) {
-	AccountNumber = accountNumber;
-}
-
-public String getIFSC_Code() {
-	return IFSC_Code;
-}
-
-public void setIFSC_Code(String iFSC_Code) {
-	IFSC_Code = iFSC_Code;
-}
-
-public String getBankName() {
-	return BankName;
-}
-
-public void setBankName(String bankName) {
-	BankName = bankName;
-}
-
-public void setAccount(Account ac) {
-	// TODO Auto-generated method stub
-	
-}
-}*/
